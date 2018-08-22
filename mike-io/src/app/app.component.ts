af60db3cd61f91ffd80d3077eba365f0c3e2560a
SoftARV <miguelaezak@gmail.com>
2018-08-22 11:17:27 -0400

    feat: navbar added

--- mike-io/src/app/app.component.ts
+++ mike-io/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mike-io';
+  menuShow: boolean = true;
+  
+  onMenuButtonClick(menuShow: boolean) {
+    this.menuShow = menuShow;
+  }
 }
---
+++ mike-io/src/app/navbar-button/navbar-button.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, HostListener, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-button',
+  templateUrl: './navbar-button.component.html',
+  styleUrls: ['./navbar-button.component.scss']
+})
+export class NavbarButtonComponent implements OnInit {
+  @Output() menuShow = new EventEmitter<boolean>();
+  isShowing: boolean = true;
+  public innerWidth: any;
+
+  ngOnInit() {
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth <= 1070) {
+      this.isShowing = false;
+      this.menuShow.emit(this.isShowing);
+    } else {
+      this.isShowing = true;
+      this.menuShow.emit(this.isShowing);
+    }
+  }
+
+  onMenuButtonClick() {
+    this.isShowing = !this.isShowing;
+    this.menuShow.emit(this.isShowing);
+  }
+}
